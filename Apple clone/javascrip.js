f3dae0b22403752b0defb430bb9df511f65ae844
Anzilna <127195080+Anzilna@users.noreply.github.com>
2024-09-06 00:00:59 +0530

    Add files via upload

---
+++ Apple clone/javascrip.js
@@ -0,0 +1,10 @@
+function scrollToSection(browse){
+    document.getElementById(browse).scrollIntoView({
+        behavior:'smooth'
+    });
+}
+function scrollToSection(latest){
+document.getElementById(latest).scrollIntoView({
+    behavior:'smooth'
+});
+}
